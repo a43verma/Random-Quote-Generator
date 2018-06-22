75cb2942990257da35a9aee351b776419c73e309
Akash Verma <akash_verma@outlook.com>
2018-06-21 20:57:57 -0400

    added the type property

--- js/script.js
+++ js/script.js
@@ -7,37 +7,51 @@ var quotes = [
     quote: 'Music is like a dream. One that I cannot hear.',
     source: 'Ludwig van Beethoven',
     citation: 'CMUSE'
+    type: 'Music'
   },
   {
     quote: 'Music gives a soul to the universe, wings to the mind, flight to the imagination and life to everything.',
     source: 'Plato',
-    citation: 'CMUSE'
+    citation: 'CMUSE',
+    type: 'Music'
   },
   {
     quote: 'One good thing about music, when it hits you, you feel no pain.',
     source: 'Bob Marley',
-    citation: 'CMUSE'
+    citation: 'CMUSE',
+    type: 'Music'
   },
   {
     quote: 'Music expresses that which cannot be said and on which it is impossible to be silent',
     source: 'Victor Hugo',
-    citation: 'CMUSE'
+    citation: 'CMUSE',
+    type: 'Music'
   },
   {
     quote: 'If I had my life to live over again, I would have made a rule to read some poetry and listen to some music at least once every week.',
     source: 'Charles Darwin',
-    citation: 'CMUSE'
+    citation: 'CMUSE',
+    type: 'Music'
   },
   {
     quote: 'Music was my refuge. I could crawl into the space between the notes and curl my back to loneliness.',
     source: 'Maya Angelou',
-    citation: 'CMUSE'
+    citation: 'CMUSE',
+    type: 'Music'
   },
   {
     quote: 'Remember to look up at the stars and not down at your feet.',
     source: 'Stepehn Hawking',
     citation: 'Time',
-    year: 2010
+    year: 2010,
+    type: 'Inspirational'
+  },
+  {
+    quote: 'I love you the more in that I believe you had liked me for my own sake and for nothing else.',
+    source: 'John Keats',
+    citation: 'Brainy Quote',
+    year: 1819,
+    type: "Love"
   }
 ];
 
@@ -56,6 +70,9 @@ function buildMessage ( ) {
   }
   if ("year" in randomQuote) {
     message += '<span class="year">' + randomQuote.year + '</span>';
+  }
+  if ("type" in randomQuote) {
+    message += '<span class="type"> (' + randomQuote.type + ')</span>';
   }
   message += '</p>'
 }
